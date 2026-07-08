54e1db1fd993c8bb03056c3b06264afd7548ce55
Mayank Kumar Yadav <mayankyadav0504@gmail.com>
2026-07-08 16:40:48 +0530

    Time: 2 ms (87.29%) | Memory: 49.5 MB (22.06%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,26 +10,56 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null)return;
-        ArrayList<ListNode> list=new ArrayList<>();
-        ListNode curr=head;
-        while(curr!=null){
-            list.add(curr);
-            curr=curr.next;
-        }
-        int left=0;
-        int right=list.size()-1;
-        while(left<right){
-            list.get(left).next=list.get(right);
-            left++;
-            if(left==right){
-                break;
+            if(head==null&&head.next==null){
+                return;
             }
-            list.get(right).next=list.get(left);
-            right--;
-        }
-        list.get(left).next=null;
-    }
+            ListNode fast=head;
+            ListNode slow=head;
+            while(fast.next!=null&&fast.next.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            ListNode newhead=slow.next;
+            slow.next=null;
+            ListNode prev=null;
+            while(newhead!=null){
+                ListNode temp=newhead.next;
+                newhead.next=prev;
+                prev=newhead;
+                newhead=temp;
+            }
+            ListNode realhead=head;
+            newhead=prev;
+            while(newhead!=null){
+                ListNode temp1=realhead.next;
+                ListNode temp2=newhead.next;
+                realhead.next=newhead;
+                newhead.next=temp1;
+                realhead=temp1;
+                newhead=temp2;
+            } 
+
+    //     if(head==null)return;
+    //     ArrayList<ListNode> list=new ArrayList<>();
+    //     ListNode curr=head;
+    //     while(curr!=null){
+    //         list.add(curr);
+    //         curr=curr.next;
+    //     }
+    //     int left=0;
+    //     int right=list.size()-1;
+    //     while(left<right){
+    //         list.get(left).next=list.get(right);
+    //         left++;
+    //         if(left==right){
+    //             break;
+    //         }
+    //         list.get(right).next=list.get(left);
+    //         right--;
+    //     }
+    //     list.get(left).next=null;
+    // }
+
     // public ListNode reorderList_1(ListNode head) {
     //     ListNode curr=head;
     //     ListNode curr_1=head;
@@ -38,5 +68,5 @@ class Solution {
     //     } 
     //     reorderList_1(curr.next);
         
-    // }
+    }
 }
